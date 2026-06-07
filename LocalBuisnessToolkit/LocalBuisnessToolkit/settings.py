7cfb47f04dc0790d6274e8fae8a1c03b1ce4c2d8
Thiroth <thirothpanha@gmail.com>
2026-06-07 15:00:05 +0700

    fix the login/signup

--- LocalBuisnessToolkit/LocalBuisnessToolkit/settings.py
+++ LocalBuisnessToolkit/LocalBuisnessToolkit/settings.py
@@ -98,30 +98,27 @@ WSGI_APPLICATION = 'LocalBuisnessToolkit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+     { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 4,  # Change from 8 to 4 characters
+        }
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # Comment out or delete the ones you don't want:
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # Removes "too similar to username" check
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # Removes "commonly used password" check
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # Removes "entirely numeric" check
+    # },
 ]
 
 
