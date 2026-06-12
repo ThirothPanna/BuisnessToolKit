1e21bddb2ba51c4926e5160cf9f609c49d73054b
Thiroth <thirothpanha@gmail.com>
2026-06-12 10:24:01 +0700

    fix the invoices that can only display more than 2

--- LocalBuisnessToolkit/feature/views.py
+++ LocalBuisnessToolkit/feature/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.utils import timezone
+from django.db.models import Q
 from datetime import datetime
 from .models import Appointment, Customer, Invoice, Notification
 from django.http import JsonResponse
@@ -119,7 +120,9 @@ def customers(request):
 def invoices(request):
     """Anyone can VIEW invoices list, but to manage need login"""
     if request.user.is_authenticated:
-        invoices = Invoice.objects.filter(customer__owner=request.user)
+        invoices = Invoice.objects.filter(
+            Q(customer__owner=request.user) | Q(user=request.user)
+        ).distinct()
         context = {
             "invoices": invoices,
             "invoices_count": invoices.count(),
